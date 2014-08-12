00d78865ad54c41de76dc6c22dfd712c5ecfed59
Dominik Richter <dominik.richter@gmail.com>
2014-08-12 14:42:27 +0200

    integrate official nginx cookbook
    this is the foundation for the hardening cookbook
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- attributes/hardening.rb
+++ attributes/hardening.rb
@@ -19,5 +19,3 @@
 # limitations under the License.
 #
 
-
-include_attribute 'nginx'
--- metadata.rb
+++ metadata.rb
@@ -23,4 +23,6 @@ description      "Configures nginx hardening"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.0"
 
+depends 'nginx', '>= 2.0.0'
+
 recipe 'nginx-hardening::default', 'configures nginx for hardening'
