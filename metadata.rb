e3100ef425680e66e780b93ddb7d27b4c0e603f1
Tim Smith <tsmith@chef.io>
2016-11-05 22:13:51 -0700

    Switch to chef_nginx
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -23,7 +23,7 @@ description      'Configures nginx hardening'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-depends 'nginx', '>= 2.0.0'
+depends 'chef_nginx', '>= 4.0'
 depends 'openssl'
 
 recipe 'nginx-hardening::default', 'configures nginx for hardening'
