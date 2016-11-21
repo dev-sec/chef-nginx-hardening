36483bf2c6644113e7dad8425509df72247946f6
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-11-21 10:37:48 +0000

    added supports specifications in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -26,6 +26,10 @@ version          "1.1.0"
 issues_url       'https://github.com/dev-sec/chef-nginx-hardening/issues'
 source_url       'https://github.com/dev-sec/chef-nginx-hardening'
 
+supports 'debian', '>= 7'
+supports 'ubuntu', '>= 12.04'
+supports 'centos', '>= 6.6'
+
 depends 'nginx', '>= 2.0.0'
 depends 'openssl'
 
